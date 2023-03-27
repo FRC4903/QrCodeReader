f6ef908d53d924a3ddae60bf3a137c43f79d17d2
KrishPatel <krishpatelsuper@gmail.com>
2023-03-27 16:01:36 -0400

    Added ability to read Pit Scout QRCODES

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ values =[""]
 if(not (os.path.exists("Teams"))):
     os.mkdir("Teams")
 
+if(not (os.path.exists("Teams\\PitScouting"))):
+    os.mkdir("Teams\\PitScouting")
+
+
 #Starts the video capture
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -29,22 +33,36 @@ while True:
 
         Vl = value.split("~~~")
         values = Vl
-        print(f"Team {Vl[0]} match {Vl[1]} has been scanned")
+        if(Vl[0] == "ps"):
+            print(f"Team {Vl[0]} match {Vl[1]} has been scanned")
 
-        if(os.path.exists(f"Teams\\{Vl[0]}.csv")):
-            with open(f"Teams\\{Vl[0]}.csv", "a", encoding='UTF8') as f:
-                w = csv.writer(f)
+            if(os.path.exists(f"Teams\\PitScouting{Vl[0]}.csv")):
+                with open(f"Teams\\PitScouting{Vl[0]}.csv", "a", encoding='UTF8') as f:
+                    w = csv.writer(f)
+                    w.writerow(Vl)
+            else:
+                open(f"Teams\\PitScouting{Vl[0]}.csv", "a").close()
+                with open(f"Teams\\PitScouting{Vl[0]}.csv", "a", encoding='UTF8') as f:
+                    w = csv.writer(f)
+                    w.writerow(["Team Number"])
                 w.writerow(Vl)
         else:
-            open(f"Teams\\{Vl[0]}.csv", "a").close()
-            with open(f"Teams\\{Vl[0]}.csv", "a", encoding='UTF8') as f:
-                w = csv.writer(f)
-                w.writerow(["Team Number","Match Number", "Scouter", "extra cones",
-                            "extra cubes", "Start Positon", "Taxi", "Balance Attempt", "Preloaded Cone",
-                            "Preload Cube", "Scored Preload", "Docked Auto", "Engaged Auto", "Upper Cone", "Mid Cone",
-                            "Lower Cone", "Upper Cube", "Mid cube", "Lower Cube", "Links",
-                            "Picked Up", "Capable to Pick Up From Station", "Capable to Pick Up From Floor","Can go over bump", "Did Defend", "Was Defended", "Time", "Docking attempts",
-                            "Endgame Docked", "Endgame Engaged", "Parked", "Auto Notes", "Teleop Notes", "Endgame Notes", "Won", "RP" ])
+            print(f"Team {Vl[0]} match {Vl[1]} has been scanned")
+
+            if(os.path.exists(f"Teams\\{Vl[0]}.csv")):
+                with open(f"Teams\\{Vl[0]}.csv", "a", encoding='UTF8') as f:
+                    w = csv.writer(f)
+                    w.writerow(Vl)
+            else:
+                open(f"Teams\\{Vl[0]}.csv", "a").close()
+                with open(f"Teams\\{Vl[0]}.csv", "a", encoding='UTF8') as f:
+                    w = csv.writer(f)
+                    w.writerow(["Team Number","Match Number", "Scouter", "extra cones",
+                                "extra cubes", "Start Positon", "Taxi", "Balance Attempt", "Preloaded Cone",
+                                "Preload Cube", "Scored Preload", "Docked Auto", "Engaged Auto", "Upper Cone", "Mid Cone",
+                                "Lower Cone", "Upper Cube", "Mid cube", "Lower Cube", "Links",
+                                "Picked Up", "Capable to Pick Up From Station", "Capable to Pick Up From Floor","Can go over bump", "Did Defend", "Was Defended", "Time", "Docking attempts",
+                                "Endgame Docked", "Endgame Engaged", "Parked", "Auto Notes", "Teleop Notes", "Endgame Notes", "Won", "RP" ])
                 w.writerow(Vl)
 
     if not ret:
@@ -52,7 +70,7 @@ while True:
         break
     font = cv.FONT_HERSHEY_COMPLEX_SMALL
     fontScale = 1.25
-    color = (50,255,50)
+    color = (255,255,255)
     thickness = 1
     frame=cv.flip(frame, 1)
     cv.putText(frame, f"scanned", (25,25), font, fontScale, color, thickness, cv.LINE_AA)
